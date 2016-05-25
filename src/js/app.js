4d6c78800029f0f524987b2989226bd5e7b0ac2f
Natasha Kerensikova <natacha@instinctive.eu>
2016-05-25 21:41:46 +0000

    Fix stupid functionification of array length

--- src/js/app.js
+++ src/js/app.js
@@ -71,7 +71,7 @@ function uploadDone() {
    var sent_key = to_send.shift().split(";")[0];
    localStorage.setItem("toSend", to_send.join("|"));
    localStorage.setItem("lastSent", sent_key);
-   if (to_send.length() === 0) {
+   if (to_send.length === 0) {
       Pebble.sendAppMessage({ "lastPosted": parseInt(sent_key, 10) });
    }
    sendHead();
